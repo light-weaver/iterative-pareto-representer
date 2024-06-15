2be42ceb4f977f04dbe223c1c23c0dbb026a3b04
Bhupinder Saini <bhupinder.s.saini@jyu.fi>
2024-06-15 18:02:30 +0300

    Changing distances to tchebycheff

--- ipa/selection.py
+++ ipa/selection.py
@@ -19,20 +19,20 @@ def DSS_numba(available: np.ndarray, taken: np.ndarray, reference_points: np.nda
 
 @njit()
 def min_dist(current_vector: np.ndarray, all_vectors: np.ndarray) -> float:
-    """Find the minimum (Eucleadean) distance between a vector and a set of vectors.
+    """Find the minimum Tchebycheff distance between a vector and a set of vectors.
 
     Args:
         current_vector: The vector to find distances from.
         all_vectors: The set of vectors to find distances to.
 
     Returns:
-        float: The distance between the current vector and the closest from all_vectors.
+        float: The Tchebycheff distance between the current vector and the closest from all_vectors.
     """
     distance = np.inf
     for vec in all_vectors:
-        curr_min_dist = np.sqrt(np.sum((current_vector - vec) ** 2))
-        if curr_min_dist < distance:
-            distance = curr_min_dist
+        curr_dist = np.max(np.abs(current_vector - vec))
+        if curr_dist < distance:
+            distance = curr_dist
     return distance
 
 
@@ -42,9 +42,9 @@ def DSS_scipy(
     reference_points: np.ndarray,
 ) -> int:
     next_ref_index = available[0]
-    min_dist = cdist(reference_points[[next_ref_index]], reference_points[taken]).min()
+    min_dist = cdist(reference_points[[next_ref_index]], reference_points[taken], metric="chebyshev").min()
     for index in available[1:]:
-        distance = cdist(reference_points[[index]], reference_points[taken]).min()
+        distance = cdist(reference_points[[index]], reference_points[taken], metric="chebyshev").min()
         if distance > min_dist:
             min_dist = distance
             next_ref_index = index
@@ -56,8 +56,11 @@ def DSS_scipy_one_liner(
     taken: np.ndarray,
     reference_points: np.ndarray,
 ) -> int:
-    assert len(available) == len(cdist(reference_points[available], reference_points[taken]).min(axis=1))
-    return available[np.argmax(cdist(reference_points[available], reference_points[taken]).min(axis=1))]
+    """One-liner implementation of the DSS algorithm using scipy."""
+
+    distances = cdist(reference_points[available], reference_points[taken]).min(axis=1)
+    assert len(available) == len(distances)
+    return available[np.argmax(distances)]
 
 
 def random_selection(
@@ -65,6 +68,7 @@ def random_selection(
     taken: np.ndarray,
     reference_points: np.ndarray,
 ) -> int:
+    """Randomly select a reference point from the available ones."""
     return np.random.choice(available)
 
 
@@ -76,11 +80,9 @@ def angle_distance(reference_point, solution, threshold):
     return min(angle1, angle2) > threshold
 
 
-def eucleadean_distance(reference_point, solution):
-    return np.linalg.norm(project(solution, reference_point) - reference_point)
-
-
 def project(solution, reference_point, normal=None):
+    """
+    Project the solution to the reference plane defined by the reference_point and the normal vector."""
     if normal is None:
         normal = np.ones_like(reference_point) / np.linalg.norm(np.ones_like(reference_point))
     perp_dist = np.dot(solution - reference_point, normal)
--- ipa/visibility.py
+++ ipa/visibility.py
@@ -18,47 +18,64 @@ def visibility(
     thickness: float = 0.05,
 ):
     next_ref_index = np.random.randint(low=0, high=len(reference_points))
-    evaluated_refs = np.zeros(len(reference_points), dtype=int)
-    bad_refs = np.zeros(len(reference_points), dtype=int)
-    k_vals = np.full(len(reference_points), dtype=float, fill_value=np.nan)
-    found_sols = []
-    success = np.zeros(max_iterations, dtype=bool)
+
+    eval_results: list[dict[str : np.ndarray | bool | int]] = []
+    evaluated_refs_idx = []
 
     # taken_subset = []
 
-    for i in range(1, max_iterations + 1):
-        evaluated_refs[next_ref_index] = i
+    iteration_counter = 1
+
+    while iteration_counter <= max_iterations:
+        bad_refs_idx = find_bad_RPs(reference_points, thickness, eval_results)
+        if eval_results:
+            eval_results[-1]["bad_fraction"] = len(bad_refs_idx) / len(reference_points)
+
+        if len(set(bad_refs_idx + evaluated_refs_idx)) == len(reference_points):
+            thickness /= 2
+            if thickness < 1e-32:
+                #print("Thickness is too small, breaking")
+                break
+            #print(f"Reducing thickness to {thickness}")
+            continue
+
         ref_point = reference_points[next_ref_index]
 
         solution, asf_val = asf_solver(ref_point)
-        k_vals[next_ref_index] = (solution - ref_point).max()
-        found_sols.append(solution)
-        assert not np.isnan(k_vals[next_ref_index])
-        # taken_subset.append(next_ref_index)
+        evaluated_refs_idx.append(next_ref_index)
 
-        # TODO: Extract distance/threshold calculation to a separate function to be injected?
-        if bad_reference_check(solution, ref_point):
-            bad_indices, _, _ = find_bad_indicesREF(solution, ref_point, reference_points, thickness)
-            # FIXME: Atleast one of the reference points will be good, but it will be marked as bad.
-            bad_refs[bad_indices] = i
-            bad_indices = np.where(bad_indices)[0]
-            """
-            if len(bad_indices) > taken_number:
-                subset = np.random.choice(bad_indices, taken_number, replace=False)
-                taken_subset.extend(subset.tolist())
-            else:
-                taken_subset.extend(bad_indices.tolist())"""
-            k_vals[bad_indices] = (solution - reference_points[bad_indices]).max(axis=1)
-            success[i - 1] = False
-        else:
-            success[i - 1] = True
+        eval_results.append(
+            {"solution": solution, "ref_point": ref_point, "success": not bad_reference_check(solution, ref_point)}
+        )
+
         # Use distance based subset selection to select next reference point
-        available = np.logical_and(bad_refs == 0, evaluated_refs == 0)
-        if available.sum() == 0:
-            break  # No more reference points to evaluate
+        available = np.ones(len(reference_points), dtype=bool)
+        available[evaluated_refs_idx] = False
+        available[bad_refs_idx] = False
         taken = ~available
         available = np.where(available)[0]
         taken = np.where(taken)[0]
+        if len(available) == 0:
+            # Handles the case when only one reference point was left to be evaluated
+            continue
+
         next_ref_index = selection_method(available, taken, reference_points)
 
-    return evaluated_refs, bad_refs, k_vals, found_sols, success
+        iteration_counter += 1
+    eval_results[-1]["bad_fraction"] = len(bad_refs_idx) / len(reference_points)
+
+    return eval_results
+
+
+def find_bad_RPs(reference_points: np.ndarray, thickness: float, eval_results: list[dict[str : np.ndarray | bool]]):
+    bad_refs_idx = []
+    for eval_result in eval_results:
+        if eval_result["success"]:
+            continue
+        bad_indices, _, _ = find_bad_indicesREF(
+            eval_result["solution"], eval_result["ref_point"], reference_points, thickness
+        )
+        bad_indices = np.where(bad_indices)[0]
+        bad_refs_idx.extend(bad_indices)
+        bad_refs_idx = list(set(bad_refs_idx))
+    return bad_refs_idx
